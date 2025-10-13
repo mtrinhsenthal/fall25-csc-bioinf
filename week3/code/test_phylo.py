4b9c4e0a3f2d6a1a4269b5991e1344a2437c11e0
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-10-12 20:22:18 -0700

    edit filepaths

--- week3/code/test_phylo.py
+++ week3/code/test_phylo.py
@@ -82,8 +82,8 @@ def test_neighbor_joining():
     assert test_tree == ref_tree
 
 
-distances: np.ndarray[int,2] = pnp.loadtxt("../tests/sequence/data/distances.txt", dtype=pnp.int64)
-with open(f'../tests/sequence/data/newick_upgma.txt', 'r') as f:
+distances: np.ndarray[int,2] = pnp.loadtxt("week3/tests/sequence/data/distances.txt", dtype=pnp.int64)
+with open(f'week3/tests/sequence/data/newick_upgma.txt', 'r') as f:
     upgma_newick = f.read().strip()
 
 # Generate the UPGMA tree
