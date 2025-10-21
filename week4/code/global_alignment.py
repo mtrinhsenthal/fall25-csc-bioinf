5bf03b6e44fea00e73c54bcf0fdbb5dbdec6ca14
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-10-20 19:18:07 -0700

    compare pairs

--- week4/code/global_alignment.py
+++ week4/code/global_alignment.py
@@ -6,7 +6,24 @@
 
 import sys
 
+def read_file(filename):
+    sequences = []
+    current_id = None
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith(">"):
+                current_id = line[1:].strip()
+                sequences.append((current_id, ""))  # tuple: (id, sequence)
+            else:
+                seq_id, seq = sequences[-1]
+                sequences[-1] = (seq_id, seq + line.strip())
+    return sequences
+
 def global_alignment(seq1, seq2, match=3, mismatch=-3, gap=-2):
+
     m = len(seq1)
     n = len(seq2)
 
@@ -64,6 +81,28 @@ def global_alignment(seq1, seq2, match=3, mismatch=-3, gap=-2):
 
     return matrix, (align1, align2, matrix[m][n])
 
+def compare_pairs(query_file, target_file):
+    queries = read_file(query_file)
+    targets = read_file(target_file)
+
+    # check for mismatch in pairs
+    if len(queries) != len(targets):
+        print("Number of queries and targets differ")
+        print(f"{len(queries)} queries, {len(targets)} targets")
+        return
+
+    for (q_id, q_seq), (t_id, t_seq) in zip(queries, targets):
+        print(f"\n=== {q_id} vs {t_id} ===")
+        alignment_matrix, (a1, a2, score) = global_alignment(q_seq, t_seq)
+        print("Score:", score)
+        print(a1)
+        print(a2)
+
+        # Optional: print the score matrix
+        # print("\nScore Matrix:")
+        # for row in alignment_matrix:
+        #     print(row)
+
 
 # Example usage:
 # A = "ABCB"
@@ -83,12 +122,13 @@ if __name__ == "__main__":
         print("Please provide filepaths to 2 sequences.")
         exit(0)
 
-    with open(sys.argv[1], 'r') as f1:
-        seq1 = ''.join(line.strip() for line in f1 if not line.startswith('>'))
+    # with open(sys.argv[1], 'r') as f1:
+    #     seq1 = ''.join(line.strip() for line in f1 if not line.startswith('>'))
 
-    with open(sys.argv[2], 'r') as f2:
-        seq2 = ''.join(line.strip() for line in f2 if not line.startswith('>'))
+    # with open(sys.argv[2], 'r') as f2:
+    #     seq2 = ''.join(line.strip() for line in f2 if not line.startswith('>'))
 
+    # matrix, (align1, align2, weight) = global_alignment(seq1, seq1)
+    # print(weight)
 
-    matrix, (align1, align2, weight) = global_alignment(seq1, seq1)
-    print(weight)
+    results = compare_pairs(sys.argv[1], sys.argv[2])
